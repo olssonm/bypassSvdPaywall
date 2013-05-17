73456d9c6e0ad20e0746a4442f45b99e0baf8754
Marcus Olsson <hello@marcusolsson.me>
2013-05-17 09:22:24 +0200

    Hotlinked to GitHub

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 (function () {
     var bsp_a = (new Date()).getTime(),
         bsp_b = document.createElement("script");
-    bsp_b.setAttribute("src", "bypassSvdPaywall.js?v=" + bsp_a), document.body.appendChild(bsp_b);
+    bsp_b.setAttribute("src", "https://raw.github.com/olssonm/bypassSvdPaywall/master/bypassSvdPaywall.js?v=" + bsp_a), document.body.appendChild(bsp_b);
 })();
